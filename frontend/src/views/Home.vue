2fe4210b40876b951cb59c3aba92dbecdf47f7b2
dongwonKang <dw3624@gmail.com>
2022-01-24 17:40:37 +0900

    Feat: 프로젝트 생성
    프런트엔드 프로젝트 폴더 생성. vue, vuex, vue-router(history mode),
    vuetify 설치.

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fill-height>
+    <v-row align="center" justify="center">
+      <v-col cols="4">
+        <h1>Linkshare</h1>
+        <h2>
+          Save content <br>
+          from <br>
+          anywhere
+        </h2>
+      </v-col>
+      
+      <!-- <v-col cols="6">
+        <h1 class="text-center">Image Placeholder</h1>
+      </v-col> -->
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  // import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      // HelloWorld,
+    },
+  }
+</script>
