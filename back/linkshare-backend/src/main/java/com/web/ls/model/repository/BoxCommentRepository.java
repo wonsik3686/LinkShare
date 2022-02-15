6c6fb183a7b21fe4ccd8207ffaa88d6f8904fdfe
정원식 <wonsic3686@gmail.com>
2022-02-15 16:53:57 +0900

    Fix: 레포 수정

--- back/linkshare-backend/src/main/java/com/web/ls/model/repository/BoxCommentRepository.java
+++ back/linkshare-backend/src/main/java/com/web/ls/model/repository/BoxCommentRepository.java
@@ -2,10 +2,13 @@ package com.web.ls.model.repository;
 
 import com.web.ls.model.entity.BoxComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface BoxCommentRepository extends JpaRepository<BoxComment, Integer> {
+
+    @Transactional
     void deleteByBoxid(int boxid);
     int countByBoxid(Integer boxid);
     List<BoxComment> findAllByBoxidOrderByRegtimeDesc(Integer boxid);
--- back/linkshare-backend/src/main/java/com/web/ls/model/repository/BoxScrapRepository.java
+++ back/linkshare-backend/src/main/java/com/web/ls/model/repository/BoxScrapRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.web.ls.model.entity.BoxScrap;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface BoxScrapRepository extends JpaRepository<BoxScrap, Integer> {
 
+    @Transactional
     void deleteByBoxid(int boxid);
     int countByBoxid(Integer boxid);
     boolean existsByBoxidAndUid(Integer boxid, Integer uid);
