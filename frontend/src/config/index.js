1569ee4feaa6b7a5bf20d9cea6d09b05a902c2e3
정원식 <wonsic3686@gmail.com>
2022-02-16 05:51:20 +0900

    Fix: base url 변경

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://localhost:9999/vue";
 // const API_BASE_URL = process.env.VUE_APP_SERVER_URL
-const API_BASE_URL = "http://3.38.246.117:8082";
+const API_BASE_URL = "http://localhost:8082";
 
 export { API_BASE_URL };
